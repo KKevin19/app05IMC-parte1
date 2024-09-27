731c67fd7f7e9e1945a920dcfdc3084e6f926259
KKevin19 <rojasocampokevindaniel@cetis50cdmx.com>
2024-09-26 20:14:28 -0600

    Update main.py

--- APP05IMC/app05IMC/main.py
+++ APP05IMC/app05IMC/main.py
@@ -2,7 +2,38 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    page.title = "calculadora de IMC"
+    page.bgcolor="purple"
+    
+    txtpeso=ft.TextField(label="ingresa tu peso")
+    txtAltura=ft.TextField(label="ingresa tu altura")
+    lblIMC=ft.Text("tu IMC es: ")
+    
+    img=ft.Image(src="https://github.com/Prof-Luis1986/Recursos/blob/main/Bascula.png",
+                width=200,
+                height=200
+                
+                )
+    btnCalcular=ft.ElevatedButton(text="calcular")
+    btnlimpiar=ft.ElevatedButton(text="limpiar")
+    
+    
+    page.add(
+        ft.Column(
+            controls=[txtpeso,
+                    txtAltura,
+                    lblIMC
+                    ],alignment="CENTER"),
+        ft.Row(
+            controls=[
+                img
+            ],alignment="CENTER"),
+        ft.row(
+            controls=[
+                btnCalcular,
+                btnlimpiar
+            ],alignment="CENTER")
+        )    
+    
 
-
-ft.app(main)
+ft.app(target=main,view=ft.AppView.WEB_BROWSER)
